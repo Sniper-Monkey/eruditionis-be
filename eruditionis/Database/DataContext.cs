8628f91def3396a447f97d2450a949a46734c4f0
yepakh <yegor46pakhomov@gmail.com>
2022-05-19 10:26:47 +0300

    Initialized project and added context

---
+++ eruditionis/Database/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using eruditionis.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eruditionis.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TestingEntity>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+        }
+
+        public DbSet<TestingEntity> TestingEntities { get; set; }
+
+    }
+    
+}
---
+++ eruditionis/Database/Models/TestingEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eruditionis.Database.Models
+{
+    public class TestingEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
